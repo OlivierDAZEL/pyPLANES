79ac178d6902b9d8e989828ca886745df833c767
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2020-04-17 22:33:59 +0200

    Headers

--- pyPLANES/utils/utils_fem.py
+++ pyPLANES/utils/utils_fem.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# utils_fem.py
+#
+# This file is part of pymls, a software distributed under the MIT license.
+# For any question, please contact one of the authors cited below.
+#
+# Copyright (c) 2020
+# 	Olivier Dazel <olivier.dazel@univ-lemans.fr>
+# 	Mathieu Gaborit <gaborit@kth.se>
+# 	Peter Göransson <pege@kth.se>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+
 from itertools import product
 import numpy as np
 
