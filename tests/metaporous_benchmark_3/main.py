fa3629c6b09c28a8301ee820b5ae79f51c49fc61
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2020-04-21 18:20:20 +0200

    benchmark 3 and 7

--- tests/metaporous_benchmark_3/main.py
+++ tests/metaporous_benchmark_3/main.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-sys.path.insert(0, "../../")
+
 from pyPLANES.model.model import Model
 from pyPLANES.classes.model_classes import ModelParameter
 from pyPLANES.utils.utils_PW import Solver_PW
@@ -13,13 +13,13 @@ from pymls import from_yaml, Solver, Layer, backing
 param = ModelParameter()
 theta_d = 0
 param.frequency = (10., 5010., 201)
-param.name_project = "metaporous_benchmark_7"
+param.name_project = "metaporous_benchmark_3"
 
 param.theta_d = theta_d
 L = 0.02
 d = 0.02
 a = 0.008
-lcar = 0.001
+lcar = 0.01
 
 param.order = 2
 # param.plot = [True, True, True, False, False, False]
@@ -42,7 +42,7 @@ c_0 = G.new_circle(L/2, d/2, a, lcar)
 matrice = G.new_surface([ll_0.tag, -c_0.tag])
 inclusion = G.new_surface([c_0.tag])
 
-G.new_physical(l_2, "condition=Transmission")
+G.new_physical(l_2, "condition=Rigid Wall")
 G.new_physical([l_1, l_3], "condition=Periodicity")
 G.new_physical(l_0, "condition=Incident_PW")
 G.new_physical(matrice, "mat=pem_benchmark_1")
