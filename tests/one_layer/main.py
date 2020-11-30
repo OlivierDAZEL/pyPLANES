e22221c71cad6c5910c763b6555b99df5d0daea6
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2020-11-30 22:55:26 +0100

    Final commit

--- tests/one_layer/main.py
+++ tests/one_layer/main.py
@@ -11,37 +11,37 @@ from pyPLANES.core.pw_problem import PwProblem
 from pyPLANES.gmsh.templates.layers import one_layer
 
 # Parameters of the simulation 
-frequencies = np.linspace(20., 5000., 1)
-theta_d = 40.000
+frequencies = np.linspace(1000., 5000., 1)
+theta_d = 0.000
 
 L = 5e-2
 d = 5e-2
-lcar = 1e-2
+lcar = 5e-2
 material = "Wwood"
 material = "melamine"
 # material = "melamine_eqf"
-# material = "Air"
+material = "Air"
 
 name_project = "one_layer"
 ml = [(material, d)]
 termination = "transmission" 
-# termination = "rigid" 
+termination = "rigid" 
 
 plot_solution = [True, True, True, False, False, False]
 # plot_solution = [False]*6
 # plot_solution = [True]*6
 
-one_layer(name_project, L, d, lcar, material, "Transmission")
-# one_layer(name_project, L, d, lcar, material, "Rigid Wall")
+# one_layer(name_project, L, d, lcar, material, "Transmission")
+one_layer(name_project, L, d, lcar, material, "Rigid Wall")
 
 global_method = PwProblem(ml=ml, name_project=name_project, theta_d=theta_d, frequencies=frequencies, plot_solution=plot_solution,termination=termination, method="global", verbose=False)
 global_method.resolution()
 
-fem = PeriodicFemProblem(name_project=name_project, name_mesh=name_project, order=5, theta_d=theta_d, frequencies=frequencies, plot_solution=plot_solution,termination=termination, verbose=False)
+fem = PeriodicFemProblem(name_project=name_project, name_mesh=name_project, order=3, theta_d=theta_d, frequencies=frequencies, plot_solution=plot_solution,termination=termination, verbose=False)
 fem.resolution()
 
-print("abs                = {}".format(1-np.abs(global_method.R)**2-np.abs(global_method.T)**2))
-# print("abs                = {}".format(1-np.abs(global_method.R)**2))
+# print("abs                = {}".format(1-np.abs(global_method.R)**2-np.abs(global_method.T)**2))
+print("abs                = {}".format(1-np.abs(global_method.R)**2))
 
 
 if any(plot_solution):
