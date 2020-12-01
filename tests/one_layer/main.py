2b7760dd1b95aa25a909804f3c79673b58b643b7
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2020-12-01 11:29:06 +0100

    priodic dof identification out of classical fem preprocess

--- tests/one_layer/main.py
+++ tests/one_layer/main.py
@@ -12,15 +12,15 @@ from pyPLANES.gmsh.templates.layers import one_layer
 
 # Parameters of the simulation 
 frequencies = np.linspace(1000., 5000., 1)
-theta_d = 0.000
+theta_d = 40.000
 
 L = 5e-2
 d = 5e-2
 lcar = 5e-2
 material = "Wwood"
 material = "melamine"
-material = "melamine_eqf"
-material = "Air"
+# material = "melamine_eqf"
+# material = "Air"
 
 name_project = "one_layer"
 ml = [(material, d)]
@@ -32,9 +32,11 @@ plot_solution = [True, True, True, False, False, False]
 # plot_solution = [True]*6
 
 bc_bottom = "Incident_PW"
-bc_bottom = "Imposed displacement"
+# bc_bottom = "Imposed displacement"
 bc_right = "Periodicity"
 bc_left = "Periodicity"
+# bc_right = "rigid"
+# bc_left = "rigid"
 bc_top = "rigid"
 
 one_layer(name_mesh=name_project, L=L, d=d, lcar=lcar, mat=material, method="FEM",  BC=[bc_bottom, bc_right, bc_top, bc_left])
@@ -48,14 +50,14 @@ fem.resolution()
 # print("abs                = {}".format(1-np.abs(global_method.R)**2-np.abs(global_method.T)**2))
 # print("abs                = {}".format(1-np.abs(global_method.R)**2))
 
-from mediapack import Air
+# from mediapack import Air
 
-omega = 2*np.pi*frequencies[0]
-k = omega/Air.c
-A = -1/np.sin(k*d)
-x = np.linspace(0,d, 500)
-p = -k*A*Air.K*np.cos(k*(x-d))
-plt.plot(x, p, 'k')
+# omega = 2*np.pi*frequencies[0]
+# k = omega/Air.c
+# A = -1/np.sin(k*d)
+# x = np.linspace(0,d, 500)
+# p = -k*A*Air.K*np.cos(k*(x-d))
+# plt.plot(x, p, 'k')
 
 if any(plot_solution):
     plt.show()
