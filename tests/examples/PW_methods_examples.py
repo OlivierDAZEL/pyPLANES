e2a630e688b80d6cac50ac3c80a865836a058aa7
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2024-04-20 08:01:26 +0200

    Add headers to examples files

--- tests/examples/PW_methods_examples.py
+++ tests/examples/PW_methods_examples.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# PW_methods_examples.py
+#
+# This file is part of pyplanes, a software distributed under the MIT license.
+# For any question, please contact one of the authors cited below.
+#
+# Copyright (c) 2024
+# 	Olivier Dazel <olivier.dazel@univ-lemans.fr>
+# 	Mathieu Gaborit <gaborit@kth.se>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# This scripts compares the 3 PW methods 
+
 import sys
 sys.path.insert(0, "../..")
 
@@ -10,26 +34,20 @@ from pyPLANES.core.pw_problem import PwProblem
 from pyPLANES.core.result import Results, Result, Test
 
 
+name_project="PW_comparisons"
 plot_solution = [True, True, True, False, False, False]
 verbose = [True, False][1]
 # Parameters of the simulation
 theta_d = 60.00000
 
-nb_layers = 2
-L = 2.e-2
 d = 2.e-2
-lcar = d/5
-nb_bloch_waves = 1
-order = 2
-
 frequency = 5e3
 
-name_project="solution"
+nb_layers = 2
 case = ["layer", "sandwich"][0]
 method_FEM = ["jap", "characteristics"][1]
 termination = ["rigid", "transmission"][1]
 material = ["Air", "Wwood", "melamine"][2]
-# material = ["Air", "Wwood", "melamine"][0]
 
 
 if case == "layer":
@@ -47,11 +65,6 @@ recursive_method.resolution()
 
 characteristic_method = PwProblem(ml=ml, name_project=name_project, theta_d=theta_d, frequencies=frequency, plot_solution=plot_solution,termination=termination, method="characteristics", verbose=verbose,save_append="a", print_result=True)
 characteristic_method.resolution()
-
-# print(f"R GM ={global_method.result.R0}")
-# print(f"R RM ={recursive_method.result.R0}")
-# print(f"R CM ={characteristic_method.result.R0}")
-
 
 if any(plot_solution):
     # plt.legend()
--- tests/examples/tube_air_FEM.py
+++ tests/examples/tube_air_FEM.py
@@ -1,3 +1,31 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# tube_air_FEM.py
+#
+# This file is part of pyplanes, a software distributed under the MIT license.
+# For any question, please contact one of the authors cited below.
+#
+# Copyright (c) 2024
+# 	Olivier Dazel <olivier.dazel@univ-lemans.fr>
+# 	Mathieu Gaborit <gaborit@kth.se>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# This scripts compares the PW and hybrid PW/FEM methods for a simple tube problem
+
+
+
+
+
 import sys
 sys.path.insert(0, "../..")
 
